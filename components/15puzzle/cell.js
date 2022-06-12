d3e6c222b6ed2a2c0f3b097f63393a3f00b84fa8
Sanjit Sarda <sanjitsarda4@gmail.com>
2022-06-11 22:44:36 -0700

    pain

--- components/15puzzle/cell.js
+++ components/15puzzle/cell.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import styles from "../styles/15puzzle.module.scss";
+import styles from "../../styles/15puzzle.module.scss";
 export default function Cell({ number, frog }) {
     var [show, style] =
         number == 0 ? ["", styles.hole] : [number, styles.piece];
